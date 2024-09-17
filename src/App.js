f86c6d9f8ab130a125fb0ad2fad36ed655527956
ozantalay <ozntalay@hotmail.com>
2024-09-17 23:47:10 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", marginTop: 30 }}>
+      <GrowingButton />
+    </div>
+  );
+}
+
+const GrowingButton = () => {
+  // KODUNUZ BURAYA GELECEK
+};
+
+export default App;
