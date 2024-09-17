6610a6a049d37810108135ab682573de17c487fc
ozantalay <ozntalay@hotmail.com>
2024-09-18 01:55:48 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import './styles.css'
 
 function App() {
   return (
@@ -10,6 +11,62 @@ function App() {
 
 const GrowingButton = () => {
   // KODUNUZ BURAYA GELECEK
+  const [btnSize,setBtnSize]=useState(100)
+  const[btnStatus,setBtnStatus]=useState(false)
+  const intervalRef=useRef(null)
+
+  const handleClick=()=>{ 
+    const minSize = 50;
+  const maxSize = 500;
+
+    if(btnStatus){
+      clearInterval(intervalRef.current);
+      intervalRef.current = setInterval(() => {
+        setBtnSize(prevSize => {
+          
+          if (prevSize <= minSize) {
+            clearInterval(intervalRef.current);
+            setBtnStatus(false);
+            return minSize;
+          }
+          return prevSize - 10;
+        });
+      }, 500);
+      setBtnStatus(false);
+    }else{
+      clearInterval(intervalRef.current);
+      intervalRef.current = setInterval(() => {
+        setBtnSize(prevSize => {
+        
+          if (prevSize >= maxSize) {
+            clearInterval(intervalRef.current);
+            setBtnStatus(true);
+            return maxSize;
+          }
+          return prevSize + 10;
+        });
+      }, 500); 
+      setBtnStatus(true);
+    }
+    
+  }
+
+
+  useEffect(() => {
+   
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+  return(
+    <>
+    <button 
+    onClick={handleClick}
+    style={{width:`${btnSize}px`, height:`${btnSize}px`}}
+    >{!btnStatus?'büyüt':'küçült'}</button>
+    </>
+  )
+
 };
 
 export default App;
